ba7cae0560ab816237eb42768e14130c6290840f
Anuranjan Srivastava <codersaty@Anuranjans-MacBook-Air.local>
2022-05-03 17:09:58 +0530

    wrote the first unit test

---
+++ app/app/tests.py
@@ -0,0 +1,10 @@
+from msilib.schema import Class
+from django.test import TestCase
+
+from app.calc import add
+
+class TestCalc(TestCase):
+
+    def test_add_numbers(self):
+        """Test that two numbers are added together"""
+        self.assertEqual(add(3,8),11)
